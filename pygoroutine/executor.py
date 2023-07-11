74ca87d2a88fe75cc1b07ce72737327198a648f9
Xiaodong Yang <yxdong.cc@outlook.com>
2023-07-11 20:22:09 +0800

    ss

--- pygoroutine/executor.py
+++ pygoroutine/executor.py
@@ -1,12 +1,70 @@
 import asyncio
-from concurrent.futures import Future
 import threading
-from typing import Awaitable, Optional, TypeVar
+from asyncio import AbstractEventLoop, Future
+from concurrent.futures import Future as ConcurrentFuture
+from typing import Any, Awaitable, Generic, Optional, TypeVar
 
 
 T = TypeVar('T')
 
 
+class CoFuture(Generic[T]):
+    def __init__(self, future: Future[T], loop: AbstractEventLoop):
+        self._future = future
+        self._loop = loop
+
+
+    def cancelled(self) -> bool:
+        return self._future.cancelled()
+    
+    
+    def done(self) -> bool:
+        return self._future.done()
+    
+    
+    def result(self, timeout: Optional[float] = None) -> T:
+        if asyncio._get_running_loop() is not None:
+            raise RuntimeError(f"Function `CoFuture.result` can't work in event loop.")
+        
+        if self._future.done():
+            return self._future.result()
+        
+        future = ConcurrentFuture()
+        self._loop.call_soon_threadsafe(self._future_callback, future)
+        return future.result(timeout)
+        
+        
+    def exception(self, timeout: Optional[float] = None) -> Optional[BaseException]:
+        if asyncio._get_running_loop() is not None:
+            raise RuntimeError(f"Function `CoFuture.exception` can't work in event loop.")
+        
+        if self._future.done():
+            return self._future.exception()
+        
+        future = ConcurrentFuture()
+        self._loop.call_soon_threadsafe(self._future_callback, future)
+        return future.exception(timeout)
+
+
+    def _future_callback(self, future: ConcurrentFuture):
+        try:
+            asyncio.futures._chain_future(self._future, future)
+        except (SystemExit, KeyboardInterrupt):
+            raise
+        except BaseException as exc:
+            if future.set_running_or_notify_cancel():
+                future.set_exception(exc)
+            raise
+
+
+    def __await__(self):
+        return self._future.__await__()
+
+
+    def __iter__(self):
+        return self._future.__iter__()
+    
+        
 class GoroutineExecutor:
     def __init__(self):
         self._lock = threading.Lock()
@@ -39,13 +97,23 @@ class GoroutineExecutor:
                 self._loop.call_soon_threadsafe(self._loop.stop)
     
     
-    def go(self, co: Awaitable[T]) -> Future[T]:
+    def go(self, coro: Awaitable[T]) -> CoFuture[T]:
+        loop = asyncio._get_running_loop()
+        if loop is None:
+            self._start()
+            loop = self._loop
+        
+        future = asyncio.ensure_future(coro, loop=loop)
+        return CoFuture(future, loop)
+
+    
+    def do(self, coro: Awaitable[T]) -> T:
+        if asyncio._get_running_loop() is not None:
+            raise RuntimeError(f"Function `do` can't work in event loop.")
+        
         self._start()
-        return asyncio.run_coroutine_threadsafe(co, self._loop)
-    
-    
-    def do(self, co: Awaitable[T]) -> T:
-        return go(co).result()
+        future = asyncio.run_coroutine_threadsafe(coro, self._loop)
+        return future.result()
 
 
 _executor = GoroutineExecutor()
--- tests/test_core.py
+++ tests/test_core.py
@@ -2,11 +2,15 @@
 
 
 import asyncio
+from collections import deque
 from concurrent.futures import Future
 import time
 from typing import List
 from pygoroutine import go, do
+from pygoroutine.executor import GoroutineExecutor
 
+
+g = GoroutineExecutor()
 
 
 
@@ -21,8 +25,15 @@ async def _work_1(idx: int):
 
 async def _work_2(future: Future):
     await asyncio.sleep(2)
-    future.cancel()
+    future.set_result('hello')
 
+
+async def _work_3(idx: int):
+    print(f'start {idx}')
+    await asyncio.sleep(2)
+    print(f'end {idx}')
+    return 'hello'
+    
 
 
 def test_goroutine_basic():
@@ -30,3 +41,76 @@ def test_goroutine_basic():
     f.result()
 
 
+
+
+
+async def get_ff():
+    g._loop.call_soon_threadsafe
+    return asyncio.Future()
+
+
+async def ff_wait(fut):
+    x = await fut
+    print(x)
+    
+    
+async def ff_prod(fut):
+    await asyncio.sleep(2)
+    fut.set_result('hello')
+    
+    
+async def kk(k):
+    z = asyncio._get_running_loop()
+    print(f'{z}')
+    for i in range(100):
+        print(k)
+
+async def aa():
+    return 'hello'
+
+async def bb():
+    x = aa()
+    print(await x)
+    
+    
+    
+async def gloop():
+    return asyncio._get_running_loop()
+
+
+async def consume(idx, x: List[int]):
+    while x:
+        d = x.pop()
+        print(f'{idx} {d}')
+
+
+
+
+async def work_4():
+    ff = []
+    for i in range(5):
+        f = go(_work_3(i))
+        ff.append(f)
+        
+    #await asyncio.sleep(3)
+    for f in ff:
+        await f
+        pass
+    
+    return 'world'
+
+if __name__ == '__main__':
+    '''
+    ff = []
+    for i in range(5):
+        f = go(_work_3(i))
+        ff.append(f)
+        
+    for f in ff:
+        f.result()
+        print(f.result())
+    '''
+    print(do(work_4()))
+    time.sleep(3)
+    
+    
